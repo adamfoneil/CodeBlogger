2de5527d99c143de612eef3a5f1b0e1a6142ce97
Adam O'Neil <adamosoftware@gmail.com>
2020-12-18 06:12:45 -0500

    renamed test project, and extracted some stuff for micropub

--- Api.Tests/GitHubIntegration.cs
+++ Api.Tests/GitHubIntegration.cs
@@ -1,3 +1,4 @@
+using Api.Tests.Helpers;
 using GitHubApi;
 using Microsoft.Extensions.Configuration;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
@@ -10,40 +11,38 @@ namespace CodeBlogger.Tests
         [TestMethod]
         public void ListPublicRepos()
         {
-            var client = GetClient();
+            var client = GetGitHubClient();
             var repos = client.ListAllPublicRepositoriesAsync().Result;
         }
 
         [TestMethod]
         public void ListMyRepos()
         {
-            var client = GetClient();
+            var client = GetGitHubClient();
             var repos = client.ListMyRepositoriesAsync(visibility: VisibilityOptions.Private).Result;
         }
 
         [TestMethod]
         public void ListRecentRepos()
         {
-            var client = GetClient();
+            var client = GetGitHubClient();
             var repos = client.ListPublicRepositoriesAsync(RepoSortOptions.Pushed, SortDirection.Descending).Result;
         }
 
         [TestMethod]
         public void ListPublicCommits()
         {
-            var client = GetClient();
+            var client = GetGitHubClient();
             var commits = client.ListCommitsAsync("Dapper.CX", 1).Result;
         }
 
         [TestMethod]
         public void ListPrivateCommits()
         {
-            var client = GetClient();
+            var client = GetGitHubClient();
             var commits = client.ListCommitsAsync("AerieHub4").Result;
         }
 
-        private GitHubApiClient GetClient() => new GitHubApiClient(Config["GitHub:UserName"], Config["GitHub:AccessToken"]);
-
-        private IConfiguration Config { get => new ConfigurationBuilder().AddJsonFile("Config/github.json").Build(); }
+        private GitHubApiClient GetGitHubClient() => new GitHubApiClient(Config.Current["GitHub:UserName"], Config.Current["GitHub:AccessToken"]);     
     }
 }
---
+++ Api.Tests/Helpers/Config.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Extensions.Configuration;
+
+namespace Api.Tests.Helpers
+{
+    public static class Config
+    {
+        public static IConfiguration Current
+        {
+            get => new ConfigurationBuilder()
+                .AddJsonFile("Config/github.json")
+                .AddJsonFile("Config/micro.blog.json").Build();
+        }
+    }
+}
